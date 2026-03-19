b8bbfb9adf150365277b00e8a159141d01bcad09
Camilo Escar <camiloescar1995@gmail.com>
2026-03-18 22:36:22 -0300

    - creacion del modulo y los componentes para la gestion de usuarios - desarrollo del listado de usuarios

---
+++ src/app/modules/users/list-users/list-users.component.ts
@@ -0,0 +1,77 @@
+import { Component } from '@angular/core';
+import { CreateUserComponent } from '../create-user/create-user.component';
+import { EditUserComponent } from '../edit-user/edit-user.component';
+import { DeleteUserComponent } from '../delete-user/delete-user.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { UsersService } from '../service/users.service';
+
+@Component({
+  selector: 'app-list-users',
+  templateUrl: './list-users.component.html',
+  styleUrls: ['./list-users.component.scss']
+})
+export class ListUsersComponent {
+search: string = '';
+  USERS: any = [];
+  isLoading$: any;
+  totalPages: number = 0;
+  currentPage: number = 0;
+  constructor(
+    public modalService: NgbModal,
+    public usersService: UsersService,
+  ) {
+
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.isLoading$ = this.usersService.isLoading$;
+    this.listUsers();
+  }
+
+  createUser(){
+    const modalRef = this.modalService.open(CreateUserComponent, { centered: true, size: 'md'});
+
+    modalRef.componentInstance.RoleC.subscribe((user:any) => {
+      this.USERS.unshift(user)
+    })
+  }
+
+  listUsers(page: number = 1){
+    this.usersService.listUsers(page, this.search).subscribe((resp: any) => {
+      console.log(resp)
+      this.USERS = resp.users
+      this.totalPages = resp.total;
+      this.currentPage = page;
+    })
+  }
+
+  loadPage($event: any) {
+    this.listUsers($event);
+  }
+
+  editUser(USER:any) {
+  const modalRef = this.modalService.open(EditUserComponent, { centered: true, size: 'md'});
+  modalRef.componentInstance.USER_SELECTED = USER;
+
+    modalRef.componentInstance.RoleE.subscribe((user:any) => {
+      let INDEX = this.USERS.findIndex((user: any) => user.id == USER.id)
+      if(INDEX != -1) {
+        this.USERS[INDEX] = user;
+      }
+    })
+  }
+
+  deleteUser(USER:any) {
+  const modalRef = this.modalService.open(DeleteUserComponent, { centered: true, size: 'md'});
+  modalRef.componentInstance.USER_SELECTED = USER;
+
+    modalRef.componentInstance.RoleD.subscribe((user:any) => {
+      let INDEX = this.USERS.findIndex((user: any) => user.id == USER.id)
+      if(INDEX != -1) {
+        this.USERS.splice(INDEX, 1);
+      }
+    })
+  }
+}
---
+++ src/app/modules/users/service/users.service.ts
@@ -0,0 +1,66 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, finalize } from 'rxjs';
+import { AuthService } from '../../auth';
+import { URL_SERVICIO } from 'src/app/config/config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  isLoading$: Observable<boolean>;
+  isLoadingSubject: BehaviorSubject<boolean>;
+
+  constructor(
+    private http: HttpClient,
+    public authservice: AuthService,
+  ) {
+    this.isLoadingSubject = new BehaviorSubject<boolean>(false);
+    this.isLoading$ = this.isLoadingSubject.asObservable();
+  }
+
+  registerUser(data: any) {
+      this.isLoadingSubject.next(true);
+      let headers = new HttpHeaders({'Authorization': 'Bearer ' + this.authservice.token});
+      let URL = URL_SERVICIO+"/users";
+      return this.http.post(URL, data, {
+        headers: headers
+      }).pipe(
+        finalize(() => this.isLoadingSubject.next(false))
+      )
+    }
+
+    listUsers(page = 1, search: string = '') {
+      this.isLoadingSubject.next(true);
+      let headers = new HttpHeaders({'Authorization': 'Bearer ' + this.authservice.token});
+      let URL = URL_SERVICIO+"/users?page="+page+"&search="+search;
+      return this.http.get(URL, {
+        headers: headers
+      }).pipe(
+        finalize(() => this.isLoadingSubject.next(false))
+      )
+    }
+
+    updateUser(ID_USER:string, data: any) {
+      this.isLoadingSubject.next(true);
+      let headers = new HttpHeaders({'Authorization': 'Bearer ' + this.authservice.token});
+      let URL = URL_SERVICIO+"/users/"+ID_USER;
+      return this.http.post(URL, data, {
+        headers: headers
+      }).pipe(
+        finalize(() => this.isLoadingSubject.next(false))
+      )
+    }
+
+    deleteUser(ID_USER:string) {
+      this.isLoadingSubject.next(true);
+      let headers = new HttpHeaders({'Authorization': 'Bearer ' + this.authservice.token});
+      let URL = URL_SERVICIO+"/users/"+ID_USER;
+      return this.http.delete(URL, {
+        headers: headers
+      }).pipe(
+        finalize(() => this.isLoadingSubject.next(false))
+      )
+    }
+}
